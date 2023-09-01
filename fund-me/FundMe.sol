6461a46cbb8afac5f466bab424e5edab88bd0150
18a6 <ar.brhl49@gmail.com>
2023-09-01 21:02:29 +0100

    feat: converted prices

--- fund-me/FundMe.sol
+++ fund-me/FundMe.sol
@@ -2,19 +2,31 @@
 pragma solidity ^0.8.18;
 
 import { DataConsumerV3 } from "./DataConsumerV3.sol";
+import { PriceConverter } from "./PriceConverter.sol";
 
 
 contract FundMe is DataConsumerV3 {
+    // using PriceConverter for uint256;
     address constant internal ETH_USD = 0x694AA1769357215DE4FAC081bf1f309aDC325306; // on sepolia
 
     constructor() DataConsumerV3(ETH_USD) {}
 
+    uint256 public minUsd = 5e18;
+    address[] public funders;
+    mapping(address funder => uint amount) funderToAmount;
+
     function fund() public payable {
-        require(msg.value > 1e18, "not enough ETH");
+        require(PriceConverter.getConversionRate(getPrice(), msg.value) >= minUsd, "not enough ETH");
+        funders.push(msg.sender);
+        funderToAmount[msg.sender] += msg.value;
     }
 
     function getPrice() public view returns(uint) {
-        (, int256 price,,,) = dataFeed.latestRoundData();
+        (, int256 price,,,) = priceFeed.latestRoundData();
         return uint(price * 1e10);
+    }
+
+    function getVersion() public view returns(uint256) {
+        return priceFeed.version();
     }
 }
